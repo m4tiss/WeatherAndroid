8b5c7550d9dd2666f23995236e098f1d801c4359
m4tiss <mateusz039@op.pl>
2024-04-05 17:33:01 +0200

    basic search

--- app/src/main/java/com/example/weatherapp/FragmentTodayWeather.kt
+++ app/src/main/java/com/example/weatherapp/FragmentTodayWeather.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -15,11 +16,12 @@ import org.json.JSONObject
 import java.io.IOException
 import java.net.URL
 
-
 class FragmentTodayWeather : Fragment() {
 
     private lateinit var temperatureTextView: TextView
     private lateinit var cityTextView: TextView
+    private lateinit var searchButton: Button
+    private lateinit var searchEditText: TextInputEditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,17 +31,24 @@ class FragmentTodayWeather : Fragment() {
         val view = inflater.inflate(R.layout.fragment_today_weather, container, false)
         temperatureTextView = view.findViewById(R.id.temperatureTextView)
         cityTextView = view.findViewById(R.id.cityTextView)
+        searchButton = view.findViewById(R.id.searchButton)
+        searchEditText = view.findViewById(R.id.searchEditText)
 
-        val city = "London"
+        val defaultCity = "Warsaw"
         val apiKey = "faefbb6cd2775b6c28ba6c3a080ead31"
-        val apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey"
+        val defaultApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=$defaultCity&appid=$apiKey"
 
-        WeatherTask().execute(apiUrl)
+
+        WeatherTask().execute(defaultApiUrl)
+
+        searchButton.setOnClickListener {
+            val city = searchEditText.text.toString()
+            val apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey"
+            WeatherTask().execute(apiUrl)
+        }
 
         return view
     }
-
-
 
     inner class WeatherTask {
         fun execute(url: String) {

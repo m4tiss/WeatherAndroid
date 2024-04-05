425a56a74581c3f6e225c40bafbbde235c9c3266
m4tiss <mateusz039@op.pl>
2024-04-05 19:43:04 +0200

    add main information

--- app/src/main/java/com/example/weatherapp/FragmentTodayWeather.kt
+++ app/src/main/java/com/example/weatherapp/FragmentTodayWeather.kt
@@ -15,11 +15,19 @@ import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.io.IOException
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
 
 class FragmentTodayWeather : Fragment() {
 
     private lateinit var temperatureTextView: TextView
     private lateinit var cityTextView: TextView
+    private lateinit var coordinatesTextView: TextView
+    private lateinit var weatherTextView: TextView
+    private lateinit var pressureTextView: TextView
+    private lateinit var timeTextView: TextView
     private lateinit var searchButton: Button
     private lateinit var searchEditText: TextInputEditText
 
@@ -31,6 +39,12 @@ class FragmentTodayWeather : Fragment() {
         val view = inflater.inflate(R.layout.fragment_today_weather, container, false)
         temperatureTextView = view.findViewById(R.id.temperatureTextView)
         cityTextView = view.findViewById(R.id.cityTextView)
+        coordinatesTextView = view.findViewById(R.id.coordinatesTextView)
+        weatherTextView = view.findViewById(R.id.weatherTextView)
+        pressureTextView = view.findViewById(R.id.pressureTextView)
+        timeTextView = view.findViewById(R.id.timeTextView)
+
+
         searchButton = view.findViewById(R.id.searchButton)
         searchEditText = view.findViewById(R.id.searchEditText)
 
@@ -61,6 +75,10 @@ class FragmentTodayWeather : Fragment() {
                 } catch (e: IOException) {
                     cityTextView.text = "-"
                     temperatureTextView.text = "-"
+                    coordinatesTextView.text = "Coordinates: -"
+                    weatherTextView.text = "Weather: -"
+                    pressureTextView.text = "Pressure: -"
+                    timeTextView.text = "Local Time: -"
                 }
             }
         }
@@ -71,15 +89,48 @@ class FragmentTodayWeather : Fragment() {
                 val main = jsonObj.getJSONObject("main")
                 val temp = main.getDouble("temp") // temperatura w kelwinach
                 val city = jsonObj.getString("name")
+                val coord = jsonObj.getJSONObject("coord")
+                val lon = coord.getDouble("lon")
+                val lat = coord.getDouble("lat")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val description = weather.getString("description")
+                val pressure = main.getInt("pressure")
 
-                // Przekształć temperaturę na stopnie Celsiusza
+
+                val timezoneOffset = jsonObj.getLong("timezone")
+                val currentTimeUTC = System.currentTimeMillis() / 1000
+                val localTime = currentTimeUTC + timezoneOffset
+
+                val sdf = SimpleDateFormat("HH:mm:ss yyyy-MM-dd ", Locale.getDefault())
+                sdf.timeZone = TimeZone.getTimeZone("UTC")
+                val sdfTime = sdf.format(localTime * 1000)
+
+
+
+
                 val tempInCelsius = temp - 273.15
-                cityTextView.text = city.toString()
-                temperatureTextView.text = "${tempInCelsius.toInt()}°C"
+                val formattedTemp = "${tempInCelsius.toInt()}°C"
+                val formattedCoord = "Longitude: $lon, Latitude: $lat"
+                val formattedWeather = "Weather: $description"
+                val formattedPressure = "Pressure: $pressure hPa"
+                val formattedTime = "Local Time: $sdfTime"
+
+                cityTextView.text = city
+                temperatureTextView.text = formattedTemp
+                coordinatesTextView.text = formattedCoord
+                weatherTextView.text = formattedWeather
+                pressureTextView.text = formattedPressure
+                timeTextView.text = formattedTime
             } catch (e: Exception) {
                 cityTextView.text = "-"
                 temperatureTextView.text = "-"
+                coordinatesTextView.text = "Coordinates: -"
+                weatherTextView.text = "Weather: -"
+                pressureTextView.text = "Pressure: -"
+                timeTextView.text = "Local Time: -"
             }
         }
+
+
     }
 }

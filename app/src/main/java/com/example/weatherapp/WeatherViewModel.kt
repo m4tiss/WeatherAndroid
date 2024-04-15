fccf7b9a52fbc7cd596ea5ecdbf804ade6af32e3
m4tiss <mateusz039@op.pl>
2024-04-15 14:32:43 +0200

    read from File

--- app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
@@ -2,9 +2,11 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.json.JSONException
 import org.json.JSONObject
 import java.io.File
 import java.io.FileWriter
@@ -19,6 +21,7 @@ class WeatherViewModel : ViewModel() {
     private val _weatherData = MutableLiveData<WeatherData>()
     val weatherData: LiveData<WeatherData>
         get() = _weatherData
+
 
     init {
         val defaultCity = _weatherData.value?.city ?: "Warsaw"
@@ -38,19 +41,29 @@ class WeatherViewModel : ViewModel() {
 
     fun fetchWeather(context: Context, unit: String) {
         val defaultCity = _weatherData.value?.city ?: "Warsaw"
-        val apiKey = "faefbb6cd2775b6c28ba6c3a080ead31"
-        val apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=$apiKey&units=$unit"
+        val networkConnection = NetworkConnection(context)
+        if (networkConnection.isNetworkAvailable()) {
 
-        GlobalScope.launch(Dispatchers.IO) {
-            try {
-                val response = URL(apiUrl).readText()
-                val weatherData = parseWeatherData(response)
-                _weatherData.postValue(weatherData)
+            val apiKey = "faefbb6cd2775b6c28ba6c3a080ead31"
+            val unit = "metric"
 
-                saveWeatherDataToFile(context,defaultCity, weatherData)
-            } catch (e: IOException) {
-                e.printStackTrace()
+            val apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=$defaultCity&appid=$apiKey&units=$unit"
+
+            viewModelScope.launch(Dispatchers.IO) {
+                try {
+                    val response = URL(apiUrl).readText()
+                    val weatherData = parseWeatherData(response)
+                    _weatherData.postValue(weatherData)
+
+
+                    saveWeatherDataToFile(context, defaultCity, weatherData)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
             }
+        } else {
+            val weatherData = loadWeatherDataFromFile(context,defaultCity)
+            _weatherData.postValue(weatherData)
         }
     }
 
@@ -132,32 +145,68 @@ class WeatherViewModel : ViewModel() {
             put("clouds", weatherData.clouds)
             put("visibility", weatherData.visibility)
         }
-
         val fileDir = context.filesDir
         val file = File(fileDir, fileName)
 
-// Create FileWriter instance for file
-        val fileWriter = FileWriter(file)
-
-// Write JSON data to file
-        fileWriter.write(json.toString())
-
-// Close FileWriter to save changes
-        fileWriter.close()
-
-        println("Dane zostały zapisane do pliku: ${file.absolutePath}")
+        try {
+            FileWriter(file).use { fileWriter ->
+                fileWriter.write(json.toString())
+                println("Dane zostały zapisane do pliku: ${file.absolutePath}")
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
 
 
         }
 
-//            val file = File(fileDir, fileName)
-//            val file = File(context.filesDir, fileName)
-//            file.writeText(json.toString())
-//            println("Zapisałem")
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//            println("NieZapisałem")
-//        }
-//    }
+    private fun loadWeatherDataFromFile(context: Context, city: String): WeatherData {
+        val fileName = "${city}_today.json"
+        val file = File(context.filesDir, fileName)
+
+        if (file.exists()) {
+            try {
+                val jsonString = file.readText()
+                val jsonObject = JSONObject(jsonString)
+
+                val weatherData = WeatherData(
+                    city = jsonObject.getString("city"),
+                    latitude = jsonObject.getDouble("latitude"),
+                    longitude = jsonObject.getDouble("longitude"),
+                    time = jsonObject.getString("time"),
+                    temperature = jsonObject.getDouble("temperature"),
+                    pressure = jsonObject.getInt("pressure"),
+                    description = jsonObject.getString("description"),
+                    humidity = jsonObject.getInt("humidity"),
+                    windSpeed = jsonObject.getDouble("windSpeed"),
+                    windDeg = jsonObject.getInt("windDeg"),
+                    clouds = jsonObject.getInt("clouds"),
+                    visibility = jsonObject.getInt("visibility")
+                )
+                println(weatherData.city)
+                println(weatherData.temperature)
+                return weatherData
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+        return WeatherData(
+            city = "",
+            latitude = 0.0,
+            longitude = 0.0,
+            time = "-",
+            temperature = 0.0,
+            pressure = 0,
+            description = "-",
+            humidity = 0,
+            windSpeed = 0.0,
+            windDeg = 0,
+            clouds = 0,
+            visibility = 0
+        )
+    }
 
 }
+
